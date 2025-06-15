10b0af5efea0c84d5b39b82a9b588c55dd9d27f8
Samuel Coveney <scove003@plattsburgh.edu>
2025-06-15 00:35:38 +0000

    working state for lesson 2, now on to lesson 3

--- scove-forge-quest-l1/src/frontend/index.jsx
+++ scove-forge-quest-l1/src/frontend/index.jsx
@@ -1,19 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { invoke } from '@forge/bridge';
-import ForgeReconciler, { Heading, Text } from '@forge/react';
-
+import ForgeReconciler, { Heading, Text, Lozenge } from '@forge/react';
+import { invoke, view } from '@forge/bridge';
 
 const App = () => {
   const [data, setData] = useState(null);
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    const getTheme = async() => {
+      const context = await view.getContext();
+      setTheme(context.theme.colorMode);
+    }
+
+  getTheme();
+  }, []);
 
   useEffect(() => {
     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
   }, []);
 
+
   return (
     <>
       <Heading size="xlarge">Hello world!</Heading>
       <Text>{data ? data : 'Loading...'}</Text>
+      <Text>Current theme: <Lozenge>{theme ? theme : 'Loading...'}</Lozenge></Text>
     </>
   );
 };
--- scove-forge-quest-l1/src/resolvers/index.js
+++ scove-forge-quest-l1/src/resolvers/index.js
@@ -5,7 +5,7 @@ const resolver = new Resolver();
 resolver.define('getText', (req) => {
   console.log(req);
 
-  return 'Hello, world!';
+  return 'Welcome to the ' + req.context.extension.space.key + ' space!';
 });
 
 export const handler = resolver.getDefinitions();
