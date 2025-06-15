9c216e52027545aac66b79cb5c9ef165e8ffeee7
Samuel Coveney <scove003@plattsburgh.edu>
2025-06-15 00:47:08 +0000

    finished part 3 or so I think

--- scove-forge-quest-l1/src/frontend/index.jsx
+++ scove-forge-quest-l1/src/frontend/index.jsx
@@ -1,15 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import ForgeReconciler, { Heading, Text, Lozenge } from '@forge/react';
-import { invoke, view } from '@forge/bridge';
+import { invoke, view, requestConfluence } from '@forge/bridge';
 
 const App = () => {
   const [data, setData] = useState(null);
   const [theme, setTheme] = useState(null);
+  const [accountID, setAccountID] = useState(null);
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    const getUserInfo = async() => {
+      if(accountID) {
+        let bodyData = `{
+          "accountIds": [
+            "${accountID}"
+          ]
+        }`;
+
+        const response = await requestConfluence(`/wiki/api/v2/users-bulk`, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: bodyData
+        });
+        if(response.status === 200) {
+          let data = await response.json();
+          console.log(data);
+
+          setName(data.results[0].publicName);
+        } else 
+        { 
+          console.log(response)
+        }
+      } else {
+        console.log("accountID is null")
+      }
+  }
+
+  getUserInfo();
+
+  }, [accountID]);
 
   useEffect(() => {
     const getTheme = async() => {
       const context = await view.getContext();
+      console.log(context);
       setTheme(context.theme.colorMode);
+      setAccountID(context.accountId);
     }
 
   getTheme();
@@ -19,10 +58,9 @@ const App = () => {
     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
   }, []);
 
-
   return (
     <>
-      <Heading size="xlarge">Hello world!</Heading>
+      <Heading size="xlarge">Hello {name ? name: 'World'}!</Heading>
       <Text>{data ? data : 'Loading...'}</Text>
       <Text>Current theme: <Lozenge>{theme ? theme : 'Loading...'}</Lozenge></Text>
     </>
